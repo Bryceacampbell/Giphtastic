939d9480b3d224778eb27ed5843bc50794ebace4
John and Kari <bryceacampbell7@gmail.com>
2019-03-26 21:07:02 -0700

    added on click event to animate giphs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
             method: "GET"
 
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
 
             var results = response.data;
 
@@ -23,33 +23,74 @@ $(document).ready(function () {
 
                 if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
+                    //creating div to hold all of the data 
                     var animalDiv = $("<div>");
                     animalDiv.addClass("gif-container");
 
+                    //parsing data and storing into variables
                     var rating = results[i].rating;
                     var animatedGif = results[i].images.fixed_height.url;
-                    var staticGif = results[i].images.fixed_height_still.url;
+                    var stillGif = results[i].images.fixed_height_still.url;
+
+                    //creating image tag and p
                     var imageTag = $("<img>");
                     var paragraphTag = $("<p>").text("Rating: " + rating);
 
-                    imageTag.attr("src", staticGif);
+                    //initially set image to still
+                    imageTag.attr("src", stillGif);
                     imageTag.attr("data-state", "still");
-                    imageTag.attr("data-still", staticGif);
+                    imageTag.attr("data-still", stillGif);
+
+                    //add animated url
                     imageTag.attr("data-animate", animatedGif);
+                    imageTag.addClass("gif-image");
+
+                    //appending image and paragraph to div
                     animalDiv.append(paragraphTag);
                     animalDiv.append(imageTag);
+
+                    //pushing data to HTML
                     $("#gif-display").prepend(animalDiv);
                 }
             }
         })
     };
 
+
+    //on click event listener to run the displaygiphs function
     $(document).on("click", ".animal-button", displayGiphs);
+
+    //on click event listener to change img source from 
+    $(document).on("click", ".gif-image", function(){
+
+        console.log("test click");
+        
+        var dataState = $(this).attr("data-state");
+
+        //if else statement to determine if gif is still or animate
+        if (dataState === "still") {
+
+            //changing img src to animate
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            console.log('animate');
+            
+        } 
+        else {
+
+            //changing img src to still
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+
+    
 
 
     function renderButtons() {
 
-        $("#animal-display").empty();
+        $("#button-display").empty();
 
         for (var i = 0; i < topics.length; i++) {
 
@@ -57,7 +98,7 @@ $(document).ready(function () {
             animalButton.addClass("animal-button");
             animalButton.attr("data-name", topics[i]);
             animalButton.text(topics[i]);
-            $("#animal-display").append(animalButton);
+            $("#button-display").append(animalButton);
 
         }
     };
